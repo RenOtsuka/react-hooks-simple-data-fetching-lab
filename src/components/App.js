0eae3bad25b82524d6d0e241b38191934c53fcb3
Ryan Delos Santos <ryansantos.delossantos121@gmail.com>
2022-03-16 10:55:39 -0500

    commit tree 7403efcbc009d047ef1a8401950fa716801b5fd6

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,29 @@
 // create your App component here
+import React, {useState, useEffect} from "react";
+
+
+function App(){
+
+    const [isLoaded, setIsLoaded] = useState(false);
+    const [randomDogImage, setRandomDogImage] = useState("");
+
+    useEffect( () => {
+        fetch(`https://dog.ceo/api/breeds/image/random`)
+        .then(resp => resp.json())
+        .then(data => {
+            setRandomDogImage(data.message)
+            setIsLoaded(true);
+        });
+    }, []);
+
+    if(!isLoaded){
+        return <p>Loading...</p>;
+    }
+
+    return (
+        <img src={randomDogImage} alt="A Random Dog"/>
+    );
+}
+
+
+export default App;
